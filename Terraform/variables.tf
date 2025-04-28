afbd14619aa9bc06c5f4fba789572410461baeac
Gaston2018 <38983253+Gaston2018@users.noreply.github.com>
2025-04-28 00:12:10 -0300

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+  description = "El token personal de GitHub"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "El usuario o la organización de GitHub donde se creará el repo"
+  type        = string
+}
