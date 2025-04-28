27b73b43d4ca106108ad89e8529f312d41aaa641
Gaston2018 <38983253+Gaston2018@users.noreply.github.com>
2025-04-28 00:11:46 -0300

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+resource "github_repository" "nuevo_repositorio" {
+  name        = "mi-repo-desde-terraform"
+  description = "Repositorio creado automáticamente con Terraform"
+  visibility  = "private" # Puede ser "public" o "private"
+
+  # Opcionales
+  has_issues   = true
+  has_wiki     = true
+  auto_init    = true  # Crea un README inicial
+}
